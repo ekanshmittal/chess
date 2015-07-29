b47646782bc71d1051e740f8c886495dad568563
ekanshmittal <ontoekansh@yahoo.com>
2015-07-29 17:03:45 +0530

    private methods moved down

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -15,29 +15,7 @@ public class Piece {
 		this.y = y;
 		captured = false;
 	}
-
-	public boolean canMoveTo(Character x, Integer y, boolean isCaptureMove) {
-		if (ChessConstants.PAWN.equals(name)) {
-			if ((Math.abs(x - this.x) == 1 && isCaptureMove) || this.x == x) {
-				if (this.y == 2 || this.y == 7) {
-					return Math.abs(this.y - y) <= 2;
-				} else {
-					return Math.abs(this.y - y) == 1;
-				}
-			} else {
-				return false;
-			}
-		} else if (ChessConstants.ROOK.equals(name)) {
-			return this.x == x || this.y == y;
-		} else if (ChessConstants.KNIGHT.equals(name)) {
-			return (Math.abs(this.x - x) == 2 && Math.abs(this.y - y) == 1)
-					|| (Math.abs(this.y - y) == 2 && Math.abs(this.x - x) == 1);
-		} else if (ChessConstants.BISHOP.equals(name)) {
-			return Math.abs(this.x - x) == Math.abs(this.y - y);
-		}
-		return true;
-	}
-
+	
 	public boolean isCaptured() {
 		return this.captured;
 	}
@@ -69,7 +47,29 @@ public class Piece {
     public void setName(String name){
         this.name = name;
     }
-
+	
+	public boolean canMoveTo(Character x, Integer y, boolean isCaptureMove) {
+		if (ChessConstants.PAWN.equals(name)) {
+			if ((Math.abs(x - this.x) == 1 && isCaptureMove) || this.x == x) {
+				if (this.y == 2 || this.y == 7) {
+					return Math.abs(this.y - y) <= 2;
+				} else {
+					return Math.abs(this.y - y) == 1;
+				}
+			} else {
+				return false;
+			}
+		} else if (ChessConstants.ROOK.equals(name)) {
+			return this.x == x || this.y == y;
+		} else if (ChessConstants.KNIGHT.equals(name)) {
+			return (Math.abs(this.x - x) == 2 && Math.abs(this.y - y) == 1)
+					|| (Math.abs(this.y - y) == 2 && Math.abs(this.x - x) == 1);
+		} else if (ChessConstants.BISHOP.equals(name)) {
+			return Math.abs(this.x - x) == Math.abs(this.y - y);
+		}
+		return true;
+	}
+	
 	@Override
 	public String toString() {
 		return name + " " + x + y;
