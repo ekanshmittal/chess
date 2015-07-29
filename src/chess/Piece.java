41ec860dd4f617ef77b01e560fb046c727319e47
ekanshmittal <ontoekansh@yahoo.com>
2015-07-29 12:09:49 +0530

    added getters and setters in piece class

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,17 +1,40 @@
-package chess.src.chess;
+package chess;
 
 /**
  * Created by test on 7/29/2015.
  */
 public class Piece {
-    char current_xCoord;
-    int current_yCoord;
-    String name;
+	private Character x;
+	private Integer y;
+	private String name;
 
-    public Piece(char current_xCoord, int current_yCoord, String name){
-        this.current_xCoord = current_xCoord;
-        this.current_yCoord = current_yCoord;
-        this.name = name;
-    }
+	public Piece(Character x, Integer y, String name) {
+		this.x = x;
+		this.y = y;
+		this.name = name;
+	}
 
+	public char getX() {
+		return x;
+	}
+
+	public void setX(char x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String toString() {
+		return name + " " + x + y;
+	}
 }
