0e78a69f19f88a2aeb5cce5848cff32a2349266b
Apoorv Umang <apoorvumang@gmail.com>
2015-07-29 12:30:47 +0530

    added constructor for Player class

--- src/chess/ChessGame.java
+++ src/chess/ChessGame.java
@@ -2,6 +2,6 @@ package chess;
 
 public class ChessGame {
 public static void main(String[] args){
-	
+	Player white = new Player(true);
 }
 }
--- src/chess/Player.java
+++ src/chess/Player.java
@@ -1,5 +1,26 @@
 package chess;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	ArrayList <Piece> pieces;
+	Player(boolean isWhite) {
+		pieces = new ArrayList<Piece> ();
+		int offset = 1;
+		if(isWhite)
+			offset = 0;
+		for(int i = 0; i < 8; ++i) {
+			pieces.add(new Piece("P", (char)('a'+ i), offset*5 + 1));
+		}
+		for(int i = 0; i < 2; ++i) {
+			pieces.add(new Piece("R", (char)('a'+ i*7), offset*7));
+			pieces.add(new Piece("N", (char)('a'+ i*5 + 1), offset*7));
+			pieces.add(new Piece("B", (char)('a'+ i*3 + 2), offset*7));
+		}
+		pieces.add(new Piece("K", (char)('a'+ 4), offset*7));
+		pieces.add(new Piece("Q", (char)('a'+ 3), offset*7));
+		for(Piece p: pieces) {
+			System.out.println(p);
+		}
+	}
 }
